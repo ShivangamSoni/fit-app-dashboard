0937509c2dba5e443728808c5ee399727dc1da8b
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-21 17:38:00 +0530

    Add: Page Component

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,10 +1,11 @@
+import Page from "../../components/layout/page";
 import DashPanel from "../../features/dashboard/dashpanel/dashpanel";
 
 const Dashboard = () => {
   return (
-    <>
+    <Page>
       <DashPanel />
-    </>
+    </Page>
   );
 };
 
