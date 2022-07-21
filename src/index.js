3b51f8d726f409f96cccde87af57c46e8e55d843
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-21 19:33:29 +0530

    Add: Users Context

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
@@ -9,9 +8,7 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
