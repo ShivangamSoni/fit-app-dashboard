20731340b3c3c2b28256030515a032bce1b41e6a
Shivangam Soni <shivangamsoni99@gmail.com>
2022-07-26 13:22:57 +0530

    Update: Responsive User Table

--- src/features/dashboard/user-table/user-table.js
+++ src/features/dashboard/user-table/user-table.js
@@ -6,38 +6,32 @@ import UserData from "../user-data/user-data";
 
 const UserTable = ({ users }) => {
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th></th>
-          <th>
-            <div>
-              <ICONS.WalkIcon />
-              <span>Steps</span>
-            </div>
-          </th>
-          <th>
-            <div>
-              <ICONS.DumbbellIcon />
-              <span>Workout</span>
-            </div>
-          </th>
-          <th>
-            <div>
-              <ICONS.NutritionIcon />
-              <span>Nutrition</span>
-            </div>
-          </th>
-          <th></th>
-        </tr>
-      </thead>
+    <div className={styles.table}>
+      <div className={styles.table__head}>
+        <div>
+          <div className={styles.header}>
+            <ICONS.WalkIcon />
+            <span>Steps</span>
+          </div>
 
-      <tbody>
+          <div className={styles.header}>
+            <ICONS.DumbbellIcon />
+            <span>Workout</span>
+          </div>
+
+          <div className={styles.header}>
+            <ICONS.NutritionIcon />
+            <span>Nutrition</span>
+          </div>
+        </div>
+      </div>
+
+      <ol className={styles.table__body}>
         {users.map((user) => (
           <UserData key={user.userId} user={user} />
         ))}
-      </tbody>
-    </table>
+      </ol>
+    </div>
   );
 };
 
